296b8ad442e65fb613eada62f8f1183f1a940f15
Rhamas01 <69786206+Rhamas01@users.noreply.github.com>
2024-06-11 07:58:20 -0500

    Add files via upload

---
+++ js/sprite.js
@@ -0,0 +1,10 @@
+let canvas = document.querySelector('canvas');
+let c = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+//create a empty array
+
+let images = [];
+images.length = 10;
